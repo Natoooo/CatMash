fb9940fbbc2a38c1467a9a260c8ee16d1f13b507
Pelozuelo <pelozuelonatalia@gmail.com>
2018-11-09 14:29:27 +0100

    install and configure all I need to start developping a React/Redux app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { App } from "./js/components/App"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import "./style/style.css"
+
+ReactDOM.render(<App />, document.getElementById("root"))
