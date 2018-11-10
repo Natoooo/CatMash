ebebb26c8d7bede877ff0bcec8736ac55590f40e
Pelozuelo <pelozuelonatalia@gmail.com>
2018-11-10 14:46:25 +0100

    implementation  of my logo and title of the app

--- front/src/js/components/App.js
+++ front/src/js/components/App.js
@@ -17,18 +17,29 @@ class App extends Component {
         <div key={key} className="col-lg-3 col-md-4 col-xs-6">
           <img
             src={cat.url}
-            className="img-thumbnail rounded-circle"
-            alt="Responsive image">
+            className="img-thumbnail bg-dark"
+            alt="Responsive cats images">
           </img>
         </div>
       )
     })
 
     return (
-      <div className="container text-center">
-        <h1>CAT MASH</h1>
+      <div className="container">
         <div className="container">
-            <div className="row mt-3">{cats}</div>
+          <img
+            src="https://image.spreadshirtmedia.com/image-server/v1/mp/designs/1015649041,width=178,height=178/yin-yang-cats-chinese-tai-chi-symbol-cool-holiday.png"
+            className="cm-logo"
+            alt="logo">
+          </img>
+        </div>
+
+        <div className="container text-center">
+          <div className="cm-title">CAT  MASH</div>
+        </div>
+
+        <div className="container">
+          <div className="row">{cats}</div>
         </div>
       </div>
     )
